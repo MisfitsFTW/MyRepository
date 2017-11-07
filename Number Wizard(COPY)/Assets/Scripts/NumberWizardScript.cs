cab009cdcdeb8bbcc1097fab8660c9083f0167eb
Jake Bugeja <b42775@mcast.org.mt>
2017-11-07 10:52:13 +0100

    Added all games to Repository

---
+++ Number Wizard(COPY)/Assets/Scripts/NumberWizardScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizardScript : MonoBehaviour {
+
+
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
+
+    // Use this for initialization
+    void Start () {
+        print("Welcome to Number Wizard!");
+        print("Pick a number between 1 and 1000!");
+
+
+        print("Is the number higher, lower or equal to " + guess + "?");
+        print("Higher: UP arrow, Lower: DOWN arrow, Equal: ENTER");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("up"))
+        {
+            print("Up Arrow pressed!");
+            min = guess;
+            guess = (min + max) / 2;
+            print("Is the number higher, lower or equal to " + guess + "?");
+            print("Higher: UP arrow, Lower: DOWN arrow, Equal: ENTER");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        // if(Input.GetKeyDown("down"))
+        {
+            print("Down Arrow pressed!");
+            max = guess;
+            guess = (min + max) / 2;
+            print("Is the number higher, lower or equal to " + guess + "?");
+            print("Higher: UP arrow, Lower: DOWN arrow, Equal: ENTER");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Enter Key pressed!");
+            print("I won!");
+        }
+
+    }
+}
---
+++ PCGExercise/Assets/UIController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public Text myQuestion;
+    public InputField myField;
+    int num1 = 0;
+    int num2 = 0;
+
+    System.Random rand = new System.Random(); // the Random class found in the System Library
+    Random unityRand = new Random(); // the Random class found in the UnityEngine Library
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GetAnswer()
+    {
+        int multiplicationAns = num1 * num2;
+
+        int userAnswer = System.Convert.ToInt32(myField.text); // read(get) - fetching the text
+
+        if (userAnswer == multiplicationAns)
+        {
+            print("Correct answer!");
+            StartGame();
+        }
+        else
+        {
+            print("Incorrect answer!");
+        }
+    }
+
+    void StartGame()
+    {
+        myField.text = "";
+        num1 = rand.Next(1, 11);
+        num2 = Random.Range(1, 11);
+
+        myQuestion.text = "What is " + num1 + " x " + num2 + "?"; // write(set) - changing the text
+    }
+}
