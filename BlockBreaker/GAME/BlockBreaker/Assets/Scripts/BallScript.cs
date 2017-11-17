d1b7501ba2aed81db1198fb7e6f8bc9ef743cecd
Jake Bugeja <b42775@mcast.org.mt>
2017-11-17 11:50:10 +0100

    We created level2 Repositioned the bricks Created a 3 hit type of brick Create audio clips for ball and bricks

---
+++ BlockBreaker/GAME/BlockBreaker/Assets/Scripts/BallScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	PaddleScript paddle;
+	Vector3 paddleBallPosDiff;//Vector 3 is a position
+	bool gameStart=false;
+
+	// Use this for initialization
+	void Start () {
+        paddle=GameObject.FindObjectOfType<PaddleScript> ();
+		paddleBallPosDiff = this.transform.position - paddle.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameStart) {//if(gameStart==false)
+			this.transform.position = paddle.transform.position + paddleBallPosDiff;
+		}
+		if (Input.GetMouseButtonDown (0) && !gameStart) {
+			gameStart = true;
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+			//using x or y, due to physics. 2f=speed on x. 10f=speed on y
+		}
+			
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ BlockBreaker/GAME/BlockBreaker/Assets/Scripts/BrickScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]//To ensure the brick has an audio source
+public class BrickScript : MonoBehaviour {
+    int timesHit;
+    public int maxHits;
+    public AudioClip crack;
+	// Use this for initialization
+	void Start () {
+
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+        print(gameObject.name+" : "+timesHit);
+        //gameObject.name is the name of the bricks(brick-brick(4))
+        if (timesHit >= maxHits)
+        {
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject);
+        }
+    }
+}
